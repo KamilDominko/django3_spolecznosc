20527390fb87e885703946835378180baa8394aa
Kamil <kamil.dominko@gmail.com>
2024-03-14 02:40:21 +0100

    Dodanie komunikatów.

--- account/views.py
+++ account/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, \
     ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 
 
 def user_login(request):
@@ -68,6 +69,11 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Uaktualnienie profilu '
+                                      'zakończyło się sukcesem.')
+        else:
+            messages.error(request, 'Wystąpił błąd podczas '
+                                    'uaktualniania profilu.')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
